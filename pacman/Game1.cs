2a58797acc7a2da1bfaa649dad4b789ec00384e1
Luke <76230394+ThatLukeDev@users.noreply.github.com>
2024-10-22 14:46:13 +0100

    sprites

--- pacman/Game1.cs
+++ pacman/Game1.cs
@@ -9,6 +9,20 @@ namespace pacman
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D _pacmantex;
+
+        Texture2D _fruit1tex;
+        Texture2D _fruit2tex;
+        Texture2D _fruit3tex;
+        Texture2D _fruit4tex;
+
+        Texture2D _ghost1tex;
+        Texture2D _ghost2tex;
+        Texture2D _ghost3tex;
+        Texture2D _ghost4tex;
+
+        Texture2D _ghost0tex;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,8 +32,6 @@ namespace pacman
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -27,7 +39,19 @@ namespace pacman
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            _pacmantex = Content.Load<Texture2D>("pacman");
+
+            _fruit1tex = Content.Load<Texture2D>("fruit1");
+            _fruit2tex = Content.Load<Texture2D>("fruit2");
+            _fruit3tex = Content.Load<Texture2D>("fruit3");
+            _fruit4tex = Content.Load<Texture2D>("fruit4");
+
+            _ghost1tex = Content.Load<Texture2D>("ghost1");
+            _ghost2tex = Content.Load<Texture2D>("ghost2");
+            _ghost3tex = Content.Load<Texture2D>("ghost3");
+            _ghost4tex = Content.Load<Texture2D>("ghost4");
+
+            _ghost0tex = Content.Load<Texture2D>("ghost0");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +59,12 @@ namespace pacman
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
